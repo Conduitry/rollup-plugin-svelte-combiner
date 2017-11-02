1e9be146d7fb3ea35fc757ff14146dbe73c09982
Conduitry <git@fur.army>
2017-11-02 19:36:10 -0400

    tidying

--- src/svelte-combiner.js
+++ src/svelte-combiner.js
@@ -18,20 +18,16 @@ export default function svelteCombinerPlugin() {
 	return {
 		load(id) {
 			if (id.endsWith('.html')) {
-				let htmlPromise = readText(id)
 				let jsId = id.slice(0, -4) + 'js'
-				let jsPromise = readText(jsId)
 				componentsJs.add(jsId)
-				return Promise.all([htmlPromise, jsPromise]).then(([html, js]) => {
-					if (js) {
-						html += `<script>${js}\nimport ${JSON.stringify(jsId)}</script>`
-					}
-					return html
-				})
-			} else if (id.endsWith('.js')) {
-				if (componentsJs.has(id)) {
-					return ''
-				}
+				return Promise.all([readText(id), readText(jsId)]).then(
+					([html, js]) =>
+						js
+							? `${html}<script>${js}\nimport ${JSON.stringify(jsId)}</script>`
+							: html
+				)
+			} else if (componentsJs.has(id)) {
+				return ''
 			}
 		},
 	}
