dc9e4fc70053d2b284e0da1392ef20265f278f50
Conduitry <git@chor.date>
2019-04-10 08:15:51 -0400

    use this.addWatchFile

--- index.js
+++ index.js
@@ -3,23 +3,22 @@ const fs = require('fs');
 const readText = filename => new Promise(res => fs.readFile(filename, (err, data) => (err ? res(null) : res(data.toString()))));
 
 module.exports = ({ extensions = ['.html', '.svelte'] } = {}) => ({
-	transform: (code, id) => {
+	transform(code, id) {
 		const extension = extensions.find(extension => id.endsWith(extension));
 		if (extension) {
 			const baseId = id.slice(0, -extension.length);
 			const jsId = baseId + '.js';
 			const cssId = baseId + '.css';
 			return Promise.all([jsId, cssId].map(readText)).then(([js, css]) => {
-				const dependencies = [];
 				if (js) {
-					dependencies.push(jsId);
+					this.addWatchFile(jsId);
 					code += `\n<script>\n${js}\n</script>\n`;
 				}
 				if (css) {
-					dependencies.push(cssId);
+					this.addWatchFile(cssId);
 					code += `\n<style>\n${css}\n</style>\n`;
 				}
-				return { code, dependencies };
+				return code;
 			});
 		}
 	},
